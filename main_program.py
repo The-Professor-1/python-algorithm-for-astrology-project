f27f26b5627d7717b9d0ea516e303a9a619aa361
Nigus Libe <97047158+The-Professor-1@users.noreply.github.com>
2025-01-30 03:04:47 +0300

    Update main_program.py

--- main_program.py
+++ main_program.py
@@ -14,7 +14,7 @@ for i in name:
 	for j in lb.fidel_value_pair().keys():
 		for k in j:
 			if k==i:
-				name_sum = (name_sum + lb.fidel_value_pair()[j])%12
+				name_sum = (name_sum + lb.fidel_value_pair()[j])%12 # 12 is standard value we use to calculate astrology sign
 for i in mother_name:
 	for j in lb.fidel_value_pair().keys():
 		for k in j:
@@ -22,14 +22,15 @@ for i in mother_name:
 				mother_name_sum=(mother_name_sum+lb.fidel_value_pair()[j])%12
 total_sum = (name_sum + mother_name_sum)%12
 title_list = list(lb.kokeb_disc_pair().keys())
-print("\n\t-----------------------------------\n\n\tየእርስዎ ኮከብ",title_list[total_sum-1] ,"ነው::")
+print(f"\n\t-----------------------------------\n\n\tየእርስዎ ኮከብ'{title_list[total_sum-1]}'ነው::")
 cont=input("\n\tስለ ኮከብዎ ማወቅ ይፈልጋሉ?  ['አዎ','አይ']\n\n\t")
 
 if cont =='አዎ':
-	print("\n\t.........................\n\t:",title_list[total_sum-1],"  :\n\t.........................\n\n")
+	print(f"\n\t.........................\n\t:'{title_list[total_sum-1]}'  :\n\t.........................\n\n")
 	specific_title = title_list[total_sum-1]
 	title_info = lb.kokeb_disc_pair()[specific_title]
 	new_info_list = title_info.split()
+# the logic below is just to customize the look of the text,it does'nt affect the value of the result.
 	for i in range(len(new_info_list)):
 	    if i != 8 and i%10==8:
 	    	print(new_info_list[i],"\n\n\t",end=" ")
